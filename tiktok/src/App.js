0e85904d65c8a6b018cef81e7f57f177a813122d
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-07-06 17:04:33 +0530

    initial commit

---
+++ tiktok/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets built a full stack tittok clone</h1>
+    </div>
+
+     
+
+  );
+}
+
+export default App;
---
+++ tiktok/src/Video.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Video.css"
+
+function Video() {
+  return (
+    <div className='video'>
+        <video src=" "></video>
+         
+    </div>
+  )
+}
+
+export default Video
